b8eacaab206278ed87a7a3c546ba037782a6546e
Satya Mohapatra <mohapsat@gmail.com>
2019-01-24 16:06:18 -0800

    updated to python 3 and completed naive bayes quiz

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,7 +15,6 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -26,6 +25,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+clf.predict(features_test)
+print("prediction time:", round(time()-t1, 3), "s")
+
+print('accuracy: %r ' % clf.score(features_test, labels_test))
+
 
 
 #########################################################
